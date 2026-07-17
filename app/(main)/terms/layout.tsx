8f5f73b1d955c766e166f75493b19223079178f5
Paul <70213368+LostPaul@users.noreply.github.com>
2026-07-17 15:22:30 +0200

    Terms of service

--- app/(main)/terms/layout.tsx
+++ app/(main)/terms/layout.tsx
@@ -1,4 +1,11 @@
 import { Article } from "@/components/article/article";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Terms of Service | Saver",
+  description:
+    "Read the terms governing access to and use of Saver's services.",
+};
 
 export default function TermsPageLayout({
   children,
