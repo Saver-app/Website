1d1577e94293587066eb962f66729ae5db8ffe09
Paul <70213368+LostPaul@users.noreply.github.com>
2026-07-17 15:01:19 +0200

    Privacy policy

--- app/(main)/privacy/layout.tsx
+++ app/(main)/privacy/layout.tsx
@@ -1,4 +1,11 @@
 import { Article } from "@/components/article/article";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Privacy Policy | Saver",
+  description:
+    "Learn how Saver collects, uses, shares, and protects personal information.",
+};
 
 export default function PrivacyPageLayout({
   children,
