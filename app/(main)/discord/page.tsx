367489767c6b3916af6156145fcf0019fe411ccf
Paul <70213368+LostPaul@users.noreply.github.com>
2026-08-15 11:23:19 +0200

    Discord link

--- app/(main)/discord/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from "next/navigation";
-
-const DISCORD_SERVER_URL = "https://discord.gg/f7BGpQFSeF";
-
-export default function DiscordPage() {
-  redirect(DISCORD_SERVER_URL);
-}
